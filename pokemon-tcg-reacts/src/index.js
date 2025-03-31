9828be191cda4fe1bd1aff8b0128bb4a1df6e9d7
John Mai <JohnNinja312@gmail.com>
2025-03-31 07:47:45 -0400

    Added index.css to index.js

--- pokemon-tcg-reacts/src/index.js
+++ pokemon-tcg-reacts/src/index.js
@@ -9,6 +9,7 @@ import Catalog from './pages/Catalog.jsx';
 import CardDetails from './pages/CardDetails.jsx';
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
+import './index.css';
 
 function App() {
   return (
