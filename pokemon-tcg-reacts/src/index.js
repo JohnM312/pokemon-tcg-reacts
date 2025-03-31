530d2d6c678ec44bd41da54ad376b8a45b614e37
John Mai <JohnNinja312@gmail.com>
2025-03-31 08:11:54 -0400

    Attempt fix in index.js

--- pokemon-tcg-reacts/src/index.js
+++ pokemon-tcg-reacts/src/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Import ReactDOM
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navigation from './components/Navigation.jsx';
 import Home from './pages/Home.jsx';
 import Rules from './pages/Rules.jsx';
 import SampleDecks from './pages/SampleDecks.jsx';
 import Catalog from './pages/Catalog.jsx';
 import CardDetails from './pages/CardDetails.jsx';
 import Footer from './components/Footer.jsx';
-import Header from './components/Header.jsx';
-import './index.css';
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div className="App">
-        <Header />
-        <Navigation />
-        <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/rules" element={<Rules />} />
@@ -25,9 +19,7 @@ function App() {
             <Route path="/catalog" element={<Catalog />} />
             <Route path="/carddetails" element={<CardDetails />} />
           </Routes>
-        </div>
         <Footer />
-      </div>
     </BrowserRouter>
   );
 }
